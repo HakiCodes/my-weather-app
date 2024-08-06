70bba6a2e7dda72fd3b0de97c051fb3f5ca47c57
Haki <nokolie4@gmail.com>
2024-08-06 19:33:56 -0400

    Created styling for current weather class

--- src/components/search/current-weather/current-weather.js
+++ src/components/search/current-weather/current-weather.js
@@ -3,11 +3,16 @@ import "./current-weather.css"
 const CurrentWeather = () => {
     return (
         <div className="weather">
-            <div classname="top">
-                <p className="city">Belgrade</p>
-                <p className="weather-description">Sunny</p>
+            <div className="top">
+                <div>
+                    <p className="city">Belgrade</p>
+                    <p className="weather-description">Sunny</p>
+                </div>
+                <img alt="weather" className="weather-icon" src="weather-icons/01d.png"></img>
             </div>
-            <img alt="weather" className="weather-icon" src="weather-icons/01d.png"></img>
+            <div className="bottom">
+            <p className="temperature">64°F</p>
+            </div>
         </div>
     );
 }
